e736737ceb117b10916bb8153a4ae3c6de7b1d9e
Alexandr Zhuchinskiy <zhunya94@gmail.com>
2016-02-09 11:25:46 +0200

    Initial Commit

---
+++ TravisCITest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravisCITest
+//
+//  Created by Serhii Onopriienko on 2/9/16.
+//  Copyright © 2016 Edsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravisCITest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisCITest
+//
+//  Created by Serhii Onopriienko on 2/9/16.
+//  Copyright © 2016 Edsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
